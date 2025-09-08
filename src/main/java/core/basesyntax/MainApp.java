0fdfab07e8e12dde787ae4c448d019bbd26c86c5
Oleksandr Hordienko <code57231@gmail.com>
2025-09-08 19:39:03 +0300

    create new class and method to output

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
-
+    public static void main(String[] args) {
+        // All executable code goes inside this method
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
